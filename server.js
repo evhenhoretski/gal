1d66a5cb8ec9249d1c6f9a5d2ad22fd08af3adac
evhenhoretski <gevina42@gmail.com>
2020-04-25 16:28:06 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+
+const server = express();
+
+const port = 3000;
+
+server
+    .get('/', (req, res) => {
+        fs.readFile('./index.html', (err, html) => {
+            if (err) {
+                throw new Error(err);
+            }
+            res.write(html);
+            res.end();
+        })
+    })
+    .get('/script.js', (req, res) => {
+        fs.readFile('./script.js', (err, html) => {
+            if (err) {
+                throw new Error(err);
+            }
+            res.write(html);
+            res.end();
+        })
+    })
+    .get('/style.css', (req, res) => {
+        fs.readFile('./style.css', (err, html) => {
+            if (err) {
+                throw new Error(err);
+            }
+            res.write(html);
+            res.end();
+        })
+    })
+    .get('/image2.jpg', (req, res) => {
+        fs.readFile('./image2.jpg', (err, html) => {
+            if (err) {
+                throw new Error(err);
+            }
+            res.write(html);
+            res.end();
+        })
+    })
+    .listen(port, () => {
+        console.log('App start on port', port);
+    });
